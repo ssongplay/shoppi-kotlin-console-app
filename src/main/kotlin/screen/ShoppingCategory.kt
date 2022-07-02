0c45ff1d9abe8bce9221940c57f487315bd2059b
송연주 <syj4700@kpu.ac.kr>
2022-07-02 23:42:13 +0900

    실습 5-2. ShoppingCart 이전화면 이동 기능 추가

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -19,8 +19,8 @@ class ShoppingCategory : Screen() {
 
         } else {
             if (categories.contains(selectedCategory)) {
-                val shoppingProductList = ShoppingProductList()
-                shoppingProductList.showProducts(selectedCategory)
+                val shoppingProductList = ShoppingProductList(selectedCategory)
+                shoppingProductList.showProducts()
 
 
             } else{
